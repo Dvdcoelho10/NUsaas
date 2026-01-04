09ae56125d1bdfa4fc3c000b58299ffd1c9dfd3e
Dvdcoelho10 <david.marcio10@gmail.com>
2026-01-04 00:03:56 -0300

    Update NUsaas.app.py

--- NUsaas.app.py
+++ NUsaas.app.py
@@ -10,7 +10,7 @@ with st.sidebar:
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
